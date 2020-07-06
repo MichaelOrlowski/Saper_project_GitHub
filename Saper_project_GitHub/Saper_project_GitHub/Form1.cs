7f90e8ad05a61bfc4f2822daf5d3fe3e7ece8813
Michał Orłowski <michal.orlowski@microsoft.wsei.edu.pl>
2020-07-06 13:30:04 +0200

    Add function to run timer Add font size for labels Add function to set Font Size even when labels are small

--- Saper_project_GitHub/Saper_project_GitHub/Form1.cs
+++ Saper_project_GitHub/Saper_project_GitHub/Form1.cs
@@ -16,5 +16,67 @@ namespace Saper_project_GitHub
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Running the stoper
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IncreaseSeconds();
+            ShowTime();
+        }
+
+        private short _seconds;
+
+        private void IncreaseSeconds()
+        {
+            _seconds++;
+        }
+
+        /// <summary>
+        /// Resseting Timer and showing it on label
+        /// </summary>
+        public void ShowTime()
+        {
+            stopwatchlabel.Text = _seconds.ToString("00");
+        }
+
+        public static int fontSize = 48;
+
+        /// <summary>
+        /// Setting up the Font Size so it always apear
+        /// even when squares are very small
+        /// </summary>
+        /// <param name="rows">Number of Rows</param>
+        /// <param name="cols">Number of Columns</param>
+        /// <returns> Returns Int Font Size</returns>
+        public static int FontSize(int rows, int cols)
+        {
+            int i = 0;
+
+
+            if (cols > 15 || rows > 15)
+            {
+                i = 15;
+            }
+            else if (cols > 10 || rows > 10)
+            {
+                i = 22;
+            }
+            else if (cols > 7 || rows > 7)
+            {
+                i = 30;
+            }
+            else
+            {
+                i = 48;
+            }
+
+
+            return fontSize = i;
+        }
+
     }
 }
