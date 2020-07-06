043dafaa7964631c2f3e6d3d1d7052a8a9569e68
Michał Orłowski <michal.orlowski@microsoft.wsei.edu.pl>
2020-07-06 13:36:46 +0200

    Adding form 2 Set start aplication from Form2

--- Saper_project_GitHub/Saper_project_GitHub/Program.cs
+++ Saper_project_GitHub/Saper_project_GitHub/Program.cs
@@ -16,7 +16,7 @@ namespace Saper_project_GitHub
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
